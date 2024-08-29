1493ffaf9d7477ef96c64898be8489e320949924
zhaojianqiang <zhaojq7687@joyowo.com>
2024-08-29 14:00:37 +0800

    initilize projects

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SegKit",
+    version="0.0.1",
+    author="zhaojianqiang",
+    author_email="zhaojianqiang@foxmail.com",
+    description="Utilities for Image Segmentation.",
+    packages=find_packages()
+)
